b0b3ecbdea24dc3af4330a5a1b772b68602571fd
leftsky <leftsky@vip.qq.com>
2022-10-07 20:07:53 +0800

    no message

--- src/helps.php
+++ src/helps.php
@@ -216,6 +216,11 @@ if (!function_exists('random_code')) {
 }
 
 if (!function_exists("decode_citycode")) {
+    /**
+     * 解析城市代码
+     * @param string $code
+     * @return string
+     */
     function decode_citycode(string $code): string
     {
         $arr = json_decode(file_get_contents(__DIR__ . "/jsons/cityCode.json"), true);
@@ -224,35 +229,81 @@ if (!function_exists("decode_citycode")) {
     }
 }
 
+if (!function_exists("get_cur_domain")) {
+    /**
+     * 获得当前的域名，【app.imgDomain】-【$_SERVER】-【domain】
+     * @return string
+     */
+    function get_cur_domain(): string
+    {
+        $domain = config("app.imgDomain");
+        if (!$domain) {
+            if (isset($_SERVER['SERVER_PORT']) && isset($_SERVER['HTTP_HOST']))
+                $domain = ((int)$_SERVER['SERVER_PORT'] === 80
+                        ? 'http://' : 'https://') . $_SERVER['HTTP_HOST'];
+            else $domain = config("domain");
+        }
+        return $domain;
+    }
+}
+
 if (!function_exists("fix_img")) {
+    /**
+     * 使用域名拼接图片路径
+     * @param string $value
+     * @return string
+     */
     function fix_img(string $value): string
     {
         if (strstr($value, "http")) return $value;
         if ($value == "") return config("app.default.image");
-        $domain = config("app.imgDomain");
-        if (!$domain) {
-            if (isset($_SERVER['SERVER_PORT']) && isset($_SERVER['HTTP_HOST']))
-                $domain = ((int)$_SERVER['SERVER_PORT'] === 80
-                        ? 'http://' : 'https://') . $_SERVER['HTTP_HOST'];
-            else $domain = config("domain");
-        }
-        return "$domain/$value";
+        $domain = get_cur_domain();
+        return "$domain$value";
+    }
+}
+
+if (!function_exists("undo_fix_img")) {
+    /**
+     * 消除图片路径中的本域名信息
+     * @param string $value
+     * @return string
+     */
+    function undo_fix_img(string $value): string
+    {
+        if ($value == "" || !strstr($value, "http")) return $value;
+        $domain = get_cur_domain();
+        return str_replace($domain, "", $value);
     }
 }
 
 if (!function_exists("fix_article")) {
+    /**
+     * 使用域名拼接替换文章内资源路径
+     * @param string $value
+     * @param string $search
+     * @return string
+     */
     function fix_article(string $value, string $search): string
     {
         if (strstr($value, "http")) return $value;
         if ($value == "") return config("app.default.image");
-        $domain = config("app.imgDomain");
-        if (!$domain) {
-            if (isset($_SERVER['SERVER_PORT']) && isset($_SERVER['HTTP_HOST']))
-                $domain = ((int)$_SERVER['SERVER_PORT'] === 80
-                        ? 'http://' : 'https://') . $_SERVER['HTTP_HOST'];
-            else $domain = config("domain");
-        }
+        $domain = get_cur_domain();
         return str_replace($search, $domain . $search, $value);
+    }
+}
+
+if (!function_exists("undo_fix_article")) {
+    /**
+     * 消除文章内的路径信息
+     * @param string $value
+     * @param string $search
+     * @return string
+     */
+    function undo_fix_article(string $value, string $search): string
+    {
+        if ($value == "" || !strstr($value, "http")) return $value;
+        $domain = get_cur_domain();
+        return str_replace($domain . $search, $search, $value);
     }
 }
 
@@ -299,7 +350,7 @@ if (!function_exists('uncamelize')) {
      * 思路:
      * 小写和大写紧挨一起的地方,加上分隔符,然后全部转小写
      */
-    function uncamelize($camelCaps, $separator='_')
+    function uncamelize($camelCaps, $separator = '_')
     {
         return strtolower(preg_replace('/([a-z])([A-Z])/', "$1" . $separator . "$2", $camelCaps));
     }
@@ -310,7 +361,8 @@ if (!function_exists('route_simple_curd')) {
      * 为一键CURD创建路由
      * @param $class
      */
-    function route_simple_curd($class) {
+    function route_simple_curd($class)
+    {
         Route::post("add", $class . "@add");
         Route::post("get", $class . "@get");
         Route::post("info", $class . "@info");
@@ -326,9 +378,9 @@ if (!function_exists('route_simple_curd')) {
 if (!function_exists("cover_up_string")) {
     /**
      * 用覆盖符覆盖字符串的一部分
-     * @param string $string    字符串
-     * @param int $start        开始
-     * @param int $len          长度
+     * @param string $string 字符串
+     * @param int $start 开始
+     * @param int $len 长度
      * @param string $separator 覆盖符，默认是*
      * @return string
      */
